45dc82ffd376cafaf2e92bb52b66c0ffb74dc23c
Lelp27 <Lelp27@github.com>
2022-08-02 14:17:42 +0900

    tag_db

--- scripts/ont_tag_test.py
+++ scripts/ont_tag_test.py
@@ -1,3 +1,3 @@
 # Sample 210701 DH5a_Plib
 
-path = '/mnt/ont/2021/210701_DH5a-PLib/dh5a-plib/20210701_0930_MC-110372_0_agw324_ac3da2d2/fastq_pass'
+data_path = '/mnt/ont/2021/210701_DH5a-PLib/dh5a-plib/20210701_0930_MC-110372_0_agw324_ac3da2d2/fastq_pass'
